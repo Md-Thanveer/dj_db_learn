7c4864413efd05f0c06b0986d242eb79d2001a41
MOHAMED THANVEER <thanveerami612@gmail.com>
2025-04-20 23:49:03 +0530

    Update admin.py

--- backend/admin.py
+++ backend/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Teacher, TeacherProfile, Course, Student, Department, Enrollment
 
-# Register your models here.
+admin.site.register(Teacher)
+admin.site.register(TeacherProfile)
+admin.site.register(Course)
+admin.site.register(Student)
+admin.site.register(Department)
+admin.site.register(Enrollment)
