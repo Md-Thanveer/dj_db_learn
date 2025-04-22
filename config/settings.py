f9b9ad2ba4f6e49693fed0312c6016d77d916ec8
MOHAMED THANVEER <thanveerami612@gmail.com>
2025-04-22 11:46:18 +0530

    add and setup cores header

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,10 @@ CSRF_TRUSTED_ORIGINS = [
     "http://dj-db-learn.onrender.com"
 ]
 
+CORS_ALLOWED_ORIGINS = [
+    "https://dj-db-learn.onrender.com",
+]
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -47,6 +51,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'backend',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -57,6 +62,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'config.urls'
